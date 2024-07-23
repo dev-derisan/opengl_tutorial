c51005a811243f262fabd3d20681b69998d97caa
dev-derisan <dev.derisan@gmail.com>
2024-07-23 19:36:08 +0900

    glew setting

--- opengl_tutorial/src/Application.cc
+++ opengl_tutorial/src/Application.cc
@@ -1,3 +1,6 @@
+#include <iostream>
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -18,6 +21,15 @@ int main(void)
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
+
+  GLenum err = glewInit();
+  if (GLEW_OK != err)
+  {
+    /* Problem: glewInit failed, something is seriously wrong. */
+    std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+  }
+
+  std::cout << glGetString(GL_VERSION) << std::endl;
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
